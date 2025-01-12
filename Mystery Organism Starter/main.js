f688386b49db0a3de87a97ceb5dc2baacd3acc6e
Aykhan Taghiyev <ayxan.tagiyev14@gmail.com>
2025-01-12 23:43:29 +0900

    add compareDNA function

--- Mystery Organism Starter/main.js
+++ Mystery Organism Starter/main.js
@@ -30,12 +30,31 @@ const pAequorFactory = (specimenNum, dna) => {
       this.dna[index] = randSelectedBase;
       
       return this.dna;
+    },
+    compareDNA(p) {
+      let count = 0;
+      for (let i = 0; i < 15; i++)  {
+        if (p.dna[i] === this.dna[i]) {
+          count++;
+        }
+      }
+      const percentage = (count / 15) * 100;
+      console.log(`specimen #${this.specimenNum} and specimen #${p.specimenNum} have ${percentage}% DNA in common.`)
     }
   }
 }
 
+//// for testing .mutate()
 // const test = pAequorFactory(0, ['A', 'T', 'C', 'G', 'A', 'T', 'C', 'G', 'A', 'T', 'C', 'G', 'A', 'T', 'C']);
 // console.log(test.mutate())
+//////
+
+//// for testing .compareDNA()
+// const test1 = pAequorFactory(0, ['A', 'T', 'C', 'G', 'A', 'T', 'C', 'G', 'A', 'T', 'C', 'G', 'A', 'T', 'C']);
+// const test2 = pAequorFactory(1, ['A', 'T', 'C', 'G', 'T', 'A', 'G', 'C', 'T', 'T', 'C', 'G', 'A', 'T', 'C']);
+// test1.compareDNA(test2);
+//// Output: specimen #0 and specimen #1 have 66.66666666666666% DNA in common.
+////// 
 
 
 
